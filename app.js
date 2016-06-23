7947d05ca3addb1f84bfec29a0ec39b488686e3c
prabhanshu98 <prabhanshu98@gmail.com>
2016-06-24 01:41:42 +0530

    Pre final commit.
    Sounds to be added. all functionality added. scorekeeping/ geticon to be
    improved.

--- app.js
+++ app.js
@@ -7,26 +7,16 @@ var num_users = 0;
 
 
 
-
-publicDir = require('path').join(__dirname, '/public');
-
-
 app.get('/', function(req, res){
 	
 	res.sendFile(__dirname + '/index.html');
 		
 });
 
-
-app.use(express.static(publicDir)); 													//serving css, js, img etc.
+publicDir = require('path').join(__dirname, '/public');
+app.use(express.static(publicDir)); 													//serving css, js, img etc. from public folder
 
 io.sockets.on('connection', function(socket){
-  
-  users.push(socket);
-  num_users = users.length;	
-  console.log("No. of players: " + num_users);
-  
-  io.emit('users', num_users);
   
   socket.on('disconnect', function() {
        
@@ -39,24 +29,75 @@ io.sockets.on('connection', function(socket){
 	   num_users = users.length;
 	   console.log("No. of players: " + num_users);
 	   		  
-  });	  
- 
+  });
   
-  socket.on('turn', function(msg){
-    io.emit('turn', msg);											//let everyone else know
+  users.push(socket);
+  num_users = users.length;	
+  console.log("No. of players: " + num_users);
+  
+  io.emit('users', num_users);
+  
+  
+  socket.on('turn', function(turn){
+	 
+    if( users[0] == socket && turn.player === 1)
+		io.emit('turnadd', turn);											//turnadd tells everyone to add the symbol
+			
 	
-});
+	else if( users[1] == socket && turn.player === 2)
+	   io.emit('turnadd', turn);
+		 	
+			
+	else
+		socket.emit('notyourturn',"It's not your turn. Wait for other player to make his move.");	
+	
+	});
 
-
-   
-   
+	
+	socket.on('geticon',function(){
+		if(socket == users[0])
+			socket.emit('icon', 'O');
+		else
+			socket.emit('icon','X');
+	});
+	
+	socket.on('win',function(turn){
+		
+		if(turn.player === 1)
+		{users[1].emit('winmsg', "Player 2 ");
+		users[0].emit('winmsg',"You ");
+		users[1].emit('updatescore', 1);
+		users[0].emit('updatescore',0);}
+		
+		if(turn.player === 2)
+		{users[0].emit('winmsg', "Player 2 ");
+		users[1].emit('winmsg',"You ");
+		users[0].emit('updatescore', 1);
+		users[1].emit('updatescore',0);}
+		
+		
+		});
+	
+	socket.on('getchance', function(turn){
+		
+		if(turn.player === 1 && socket == users[0])
+		{ socket.emit('mychance','.player1');
+			users[1].emit('mychance','.player2');}
+		
+		if(turn.player === 2 && socket == users[1])
+		{ socket.emit('mychance','.player1');
+			users[0].emit('mychance','.player2');}
+		
+		
+	});
+	
 
 });
 
 
 var port = 8080;
 
-http.listen( port , function(){
+http.listen( process.env.VCAP_APP_PORT || port , function(){
   console.log('listening on port : ' + port);
 });
 
--- public/js/game.js
+++ public/js/game.js
@@ -1,15 +1,39 @@
 
 $(document).ready(function(){
 	var socket = io();
-	var score = 0;
+	var score1 = 0, score2 = 0,ties=0;
+	var squares;
+	var winner =false;
+	var moves = 0;
+	var icon, iconother;
+	var turn = {
+		player : 1,
+		square: 0
+		//GLOBAL variable
+	}
+	
+	var resetboard = function(){
+		
+		$('.square').empty('p');
+		moves = 0;
+		
+	}
 	
 	socket.on('users', function(num_users){
 	   if(num_users == 1)
 	   { $('h1').remove();
 		 $('body').append('<h1>Waiting for second player</h1>');} 	
 	   if(num_users == 2)
-	   { $('h1').remove();   
+	   { $('h1').remove(); 
    
+		socket.emit('geticon', 'abcde');
+		socket.on('icon',function(msg){
+			icon = msg;
+			if(icon == "X")
+				iconother="O";
+			else
+				iconother="X";
+		
 		$('body').append('\
 		<div class="game">\
 			<div class="board">\
@@ -23,8 +47,27 @@ $(document).ready(function(){
 				<div class="square bottom"></div>\
 				<div class="square bottom right"></div>\
 			</div>\
-		</div>\
-	   <div class="scores"><p>svevevefffffff</p></div>' );  }
+			</div>\
+	   <div class="scores p1">\
+<p class="player1"><span class="p1">You</span><span class="p2">Player 1</span> (<span class="x">' + icon + '</span>)<span class="score"> '+ score1 + '</span></p>\
+<p class="ties">Ties<span class="score">' + ties + '</span></p>\
+<p class="player2"><span class="p1">Player 2</span><span class="p2">Player 2</span> (<span class="o">'+ iconother + '</span>)<span class="score">' + score2 + '</span></p>\
+</div>\
+	   <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">\
+  <div class="vertical-alignment-helper">\
+  <div class="modal-dialog  vertical-align-center" role="document">\
+    <div class="modal-content">\
+      <div class="modal-header">\
+        <button type="button" class="close" data-dismiss="modal" aria-label="Close">\
+          <span aria-hidden="true">&times;</span>\
+        </button>\
+      </div>\
+	  </div>\
+	 </div>\
+    </div>\
+  </div>\
+	   </div>');  });	
+	   }
 	   
 	   if(num_users >=3)
 	   {	
@@ -37,28 +80,21 @@ $(document).ready(function(){
 	socket.on('reload', function(msg){
 	   
 	   location.reload(true);
+	   
    });	
    
-   
-	
-	var turn = {
-		player : 1,
-		name : "Player",
-		square: 0
-		//GLOBAL variable
-	}
-	
+  
 	
 	
 	$("body").on("click", "div.square", function(event){
 		
 			turn.square = $(this).index();
-			
+				
 			socket.emit('turn', turn);
 	});
 	
 
-	socket.on('turn', function(turnval){
+	socket.on('turnadd', function(turnval){
 		
 		//square number is index. squares begin from 0 not from one.
 		square_num = turnval.square;
@@ -70,39 +106,90 @@ $(document).ready(function(){
 				clicked_square.append('<p class="O">o</p>');
 			else
 				clicked_square.append('<p class="X">x</p>');
-		turn.player = 3-turn.player;					//GLOBAL change
-		}
 		
-		socket.emit('checkwin', 'abcde' );					
 		
-   });
-	
-   
-  
-	socket.on('checkwin', function(){
-		console.log("heyhyehey!");
-		var squares = $(board).children( ".square" );
-		console.log('squares');
+		//check if there's a winner
+		
+		
+		squares = $(".square").toArray();
+		
 		 winner  = false;
-                if (squares[0].innerHTML === mark && squares[1].innerHTML === mark && squares[2].innerHTML === mark) {
+		 
+                if (squares[0].innerHTML && squares[0].innerHTML === squares[1].innerHTML  && squares[1].innerHTML === squares[2].innerHTML) {
                   winner = true;
-                } else if (squares[3].innerHTML === mark && squares[4].innerHTML === mark && squares[5].innerHTML === mark) {
+                } else if (squares[3].innerHTML && squares[3].innerHTML ===  squares[4].innerHTML && squares[4].innerHTML ===  squares[5].innerHTML ) {
                   winner = true;
-                } else if (squares[6].innerHTML === mark && squares[7].innerHTML === mark && squares[8].innerHTML === mark) {
+                } else if (squares[6].innerHTML && squares[6].innerHTML ===  squares[7].innerHTML && squares[7].innerHTML ===  squares[8].innerHTML ) {
                   winner = true;
-                } else if (squares[0].innerHTML === mark && squares[3].innerHTML === mark && squares[6].innerHTML === mark) {
+                } else if (squares[0].innerHTML && squares[0].innerHTML ===  squares[3].innerHTML && squares[3].innerHTML ===  squares[6].innerHTML ) {
                   winner = true;
-                } else if (squares[1].innerHTML === mark && squares[4].innerHTML === mark && squares[7].innerHTML === mark) {
+                } else if (squares[1].innerHTML && squares[1].innerHTML ===  squares[4].innerHTML && squares[4].innerHTML ===  squares[7].innerHTML ) {
                   winner = true;
-                } else if (squares[2].innerHTML === mark && squares[5].innerHTML === mark && squares[8].innerHTML === mark) {
+                } else if (squares[2].innerHTML && squares[2].innerHTML ===  squares[5].innerHTML && squares[5].innerHTML ===  squares[8].innerHTML ) {
                   winner = true;
-                } else if (squares[0].innerHTML === mark && squares[4].innerHTML === mark && squares[8].innerHTML === mark) {
+                } else if (squares[0].innerHTML && squares[0].innerHTML ===  squares[4].innerHTML && squares[4].innerHTML ===  squares[8].innerHTML ) {
                   winner = true;
-                } else if (squares[2].innerHTML === mark && squares[4].innerHTML === mark && squares[6].innerHTML === mark) {
+                } else if (squares[2].innerHTML && squares[2].innerHTML ===  squares[4].innerHTML && squares[4].innerHTML ===  squares[6].innerHTML ) {
                   winner = true;
                 }
-                return winner;
+         
+		if(winner === true)	
+		{socket.emit('win', turn);turn.player = 3-turn.player;	//Winner gets first move
 		
+		console.log(moves);}
+		
+		else if(moves === 8)
+			{ $('.modal-title').remove();
+				$('.modal-header').append('<h3 class="modal-title">It\'s a draw</h3> ' );
+				$('#myModal').modal('show');
+				resetboard();
+				ties++;
+				$('.ties > .score').empty();
+				$('.ties > .score').append(ties);}
+				
+		else{
+			turn.player = 3-turn.player;					//GLOBAL change
+		moves++;
+		
+		socket.emit('getchance',turn);}
+		
+	}	
+	
+	});
+	
+	
+   socket.on('mychance',function(msg)
+				{	
+					$('.player1').css("color", 'black');
+					$('.player2').css('color', 'black');
+					$(msg).css('color','#fff');
+		});
+		
+   socket.on('notyourturn', function(msg){
+	  
+		 $('.modal-title').remove();
+		 $('.modal-header').append('<h3 class="modal-title">' + msg + '</h3> ' );
+		 $('#myModal').modal('show');
+
+   });
+  
+	socket.on('winmsg', function(msg){
+		 $('.modal-title').remove();
+		 $('.modal-header').append('<h3 class="modal-title">' + msg + 'won</h3> ' );
+		 $('#myModal').modal('show');
+		 socket.emit('checkscore', turn);
+		 setTimeout(resetboard, 3000);
+		 	
+	});
+	
+	socket.on('updatescore', function(msg){
+		
+		if(!msg) score1++;
+		if(msg) score2++;
+		$('.player1 >.score').empty();
+		$('.player2 >.score').empty();
+		$('.player1 > .score ').append(score1/2);
+		$('.player2 > .score ').append(score2/2);
 	});
 
 });
